63c7c6a6d2d06a5b5d5fc90941887e692f28d970
littleflow3r <36025078+littleflow3r@users.noreply.github.com>
2019-02-27 14:59:22 +0900

    Update model.py

--- model.py
+++ model.py
@@ -3,7 +3,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 class attbilstm(nn.Module):
-    def __init__(self, vocab_size, config):
+    def __init__(self, vocab_size, config, vec=None):
         super().__init__()
         self.hidden_dim = config['hidden_dim']
         self.batch_size = config['batch_size']
@@ -11,6 +11,9 @@ class attbilstm(nn.Module):
         self.gpu = config['gpu']
         
         self.embedding = nn.Embedding(vocab_size, config['emb_dim'])
+        if vec is not None:
+            self.embedding.weight.data.copy_(vec) #load pretrained
+            #self.embedding.weight.requires_grad = False #non-trainable
         self.encoder = nn.LSTM(config['emb_dim'], config['hidden_dim'], num_layers=config['nlayers'], bidirectional=config['bidir'], dropout=config['dropout'])
         self.fc = nn.Linear(config['hidden_dim'], config['out_dim'])
         self.dropout = nn.Dropout(config['dropout'])
@@ -28,6 +31,20 @@ class attbilstm(nn.Module):
         
         return new_hidden
      
+    def attnetwork1(self, encoder_out, final_hidden):
+        print ('ddddd')
+        #print (encoder_out.size())
+        print ('fh:', final_hidden)
+        merged_state = torch.cat([s for s in final_hidden],1)
+        merged_state = (merged_state.unsqueeze(2))
+        #print (merged_state.size())
+        encoder_out = encoder_out.permute(1,0,2)
+        print (encoder_out.size(), merged_state.size())
+        weights = torch.bmm(encoder_out, merged_state)
+        weights = F.softmax(weights.squeeze(2)).unsqueeze(2)
+        new_hidden = torch.bmm(torch.transpose(encoder_out,1,2), weight).squeeze(2)
+        print ('BOTTOM:',new_hidden, new_hidden.shape)
+        return new_hidden
     
     def forward(self, sequence):
         emb_input = self.embedding(sequence)    
@@ -38,6 +55,7 @@ class attbilstm(nn.Module):
         fbhn = (hn[-2,:,:]+hn[-1,:,:]).unsqueeze(0)
         #print (fbhn.shape, fbout.shape)
         attn_out = self.attnetwork(fbout, fbhn)
+        #attn1_out = self.attnetwork1(output, hn)
         logits = self.fc(attn_out)
         return logits
         
